2961625dbeb8c19eebd76dceb9e15afb748bb82f
Christopher Speller <crspeller@gmail.com>
2016-12-08 08:36:34 -0500

    Fixes for user entity get channel

--- cmd/loadtest/user_entity_get_channel.go
+++ cmd/loadtest/user_entity_get_channel.go
@@ -16,6 +16,7 @@ type UserEntityGetChannel struct {
 
 type UserEntityGetChannelConfiguration struct {
 	GetFrequencySeconds int
+	UseEtags            bool
 }
 
 func NewUserEntityGetChannelConfig() UserEntityGetChannelConfiguration {
@@ -45,6 +46,10 @@ func (me *UserEntityGetChannel) Start() {
 
 	var getChannelCount int64 = 0
 
+	getChannelEtag := ""
+	getPostsEtag := ""
+	getChannelStatsEtag := ""
+
 	me.SendStatusActive("Getting channels")
 	for {
 		select {
@@ -54,28 +59,31 @@ func (me *UserEntityGetChannel) Start() {
 		case <-getTicker.C:
 			channel := me.GetChannelBasedOnActionCount(getChannelCount)
 			me.Client.SetTeamId(channel.TeamId)
-			if _, err := me.Client.GetChannel(channel.Id, ""); err != nil {
+			if result, err := me.Client.GetChannel(channel.Id, getChannelEtag); err != nil {
 				me.SendStatusError(err, "Failed to get channel: "+channel.Id)
 			} else {
-				me.SendStatusActionSend("Got channel")
+				if me.Config.UseEtags && result.Etag != "" {
+					getChannelEtag = result.Etag
+				}
+				me.SendStatusActionSend("Got channel. Etag: " + getChannelEtag)
 			}
 
-			if _, err := me.Client.GetChannels(""); err != nil {
-				me.SendStatusError(err, "Failed to get channels")
-			} else {
-				me.SendStatusActionSend("Got Channels")
-			}
-
-			if _, err := me.Client.GetPosts(channel.Id, 0, 60, ""); err != nil {
+			if result, err := me.Client.GetPosts(channel.Id, 0, 60, getPostsEtag); err != nil {
 				me.SendStatusError(err, "Failed to get posts: "+channel.Id)
 			} else {
-				me.SendStatusActionSend("Got Posts")
+				if me.Config.UseEtags && result.Etag != "" {
+					getPostsEtag = result.Etag
+				}
+				me.SendStatusActionSend("Got Posts. Etag: " + getPostsEtag)
 			}
 
-			if _, err := me.Client.GetChannelStats(channel.Id, ""); err != nil {
+			if result, err := me.Client.GetChannelStats(channel.Id, getChannelStatsEtag); err != nil {
 				me.SendStatusError(err, "Failed to get channel stats: "+channel.Id)
 			} else {
-				me.SendStatusActionSend("Got channel stats")
+				if me.Config.UseEtags {
+					getChannelStatsEtag = result.Etag
+				}
+				me.SendStatusActionSend("Got channel stats. Etag: " + getChannelStatsEtag)
 			}
 
 			if _, err := me.Client.UpdateLastViewedAt(channel.Id, true); err != nil {
@@ -83,6 +91,8 @@ func (me *UserEntityGetChannel) Start() {
 			} else {
 				me.SendStatusActionSend("Updated last viewed at")
 			}
+
+			getChannelCount += 1
 		}
 	}
 }
