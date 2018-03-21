5f5794a382570c1f60bd00a8dcea7fdb6d263ecf
Christopher Brown <ccbrown112@gmail.com>
2018-03-20 21:07:05 -0500

    change default session duration to one hour

--- ops/aws.go
+++ ops/aws.go
@@ -5,10 +5,18 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/aws/stscreds"
 )
+
+func init() {
+	// There may be a way to do this in LoadAWSConfig, rather than by modifying this global, but
+	// it's not obvious to me what it is.
+	stscreds.DefaultDuration = time.Hour
+}
 
 func LoadAWSConfig() (aws.Config, error) {
 	return external.LoadDefaultAWSConfig(external.WithMFATokenFunc(MFATokenPrompt))
