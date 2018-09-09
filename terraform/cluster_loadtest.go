c704023d5cd2811bb8bfe6b7368ffbca2f12ce6f
Jesse Hallam <jesse.hallam@gmail.com>
2018-09-08 20:48:01 -0400

    route loadtest logs through logrus, ensuring flush

--- terraform/cluster_loadtest.go
+++ terraform/cluster_loadtest.go
@@ -2,7 +2,9 @@ package terraform
 
 import (
 	"bufio"
+	"encoding/json"
 	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"sync"
@@ -14,6 +16,84 @@ import (
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
+
+type logrusWriter struct {
+	*io.PipeWriter
+
+	reader *io.PipeReader
+	done   chan bool
+}
+
+func newLogrusWriter(logger logrus.FieldLogger) *logrusWriter {
+	reader, writer := io.Pipe()
+	done := make(chan bool)
+
+	go func() {
+		// Log lines one at a time and mapped into logrus calls for visual clarity. The
+		// raw output written to the file will be unaffected.
+		scanner := bufio.NewScanner(reader)
+		for scanner.Scan() {
+			var jsonText interface{}
+			if err := json.Unmarshal(scanner.Bytes(), &jsonText); err != nil {
+				logger.Info(scanner.Text())
+			} else if jsonTextMap, ok := jsonText.(map[string]interface{}); !ok {
+				logger.Info(scanner.Text())
+			} else {
+				level, _ := jsonTextMap["level"].(string)
+				msg, _ := jsonTextMap["msg"].(string)
+
+				// Clean up unused fields
+				delete(jsonTextMap, "msg")
+				delete(jsonTextMap, "level")
+				delete(jsonTextMap, "ts")
+				delete(jsonTextMap, "caller")
+
+				// Special case timings output. This will still be available
+				// in the results for use by ltparse.
+				if msg == "Timings" {
+					jsonTextMap["timings"] = "(omitted)"
+				}
+
+				loggerWithFields := logger.WithFields(logrus.Fields(jsonTextMap))
+
+				switch level {
+				case "debug":
+					loggerWithFields.Debug(msg)
+				case "info":
+					loggerWithFields.Info(msg)
+				case "warn":
+					loggerWithFields.Warn(msg)
+				case "error":
+					fallthrough
+				default:
+					loggerWithFields.Error(msg)
+				}
+			}
+		}
+		if err := scanner.Err(); err != nil {
+			logrus.Errorf("failed to scan and log: %s", err.Error())
+
+			// Drain the reader, otherwise the ssh session won't end.
+			// TODO: Really, we still want to just dump a really long line, but this is a test.
+			io.Copy(ioutil.Discard, reader)
+		}
+		close(done)
+	}()
+
+	return &logrusWriter{
+		PipeWriter: writer,
+		reader:     reader,
+		done:       done,
+	}
+}
+
+func (w *logrusWriter) Close() error {
+	w.PipeWriter.Close()
+	w.reader.Close()
+	<-w.done
+
+	return nil
+}
 
 func (c *Cluster) loadtestInstance(logger logrus.FieldLogger, addr string, instanceNum int, configFile []byte) error {
 	client, err := sshtools.SSHClient(c.SSHKey(), addr)
@@ -48,18 +128,19 @@ func (c *Cluster) loadtestInstance(logger logrus.FieldLogger, addr string, insta
 	if err != nil {
 		return errors.Wrap(err, "Unable to create loadtest results file.")
 	}
+	defer outfile.Close()
 
-	loggerPipeReader, loggerPipeWriter := io.Pipe()
+	// Unlike os.Exec, there's a data race if session.Stdout == session.Stderr.
+	// https://github.com/golang/go/issues/5582. Avoid this by using a pipe, which is safe
+	// when written to concurrently.
+	sessionPipeReader, sessionPipeWriter := io.Pipe()
+	session.Stdout = sessionPipeWriter
+	session.Stderr = sessionPipeWriter
+	defer sessionPipeWriter.Close()
 
-	session.Stdout = io.MultiWriter(outfile, loggerPipeWriter)
-	session.Stderr = session.Stdout
-
-	go func() {
-		scanner := bufio.NewScanner(loggerPipeReader)
-		for scanner.Scan() {
-			logger.Info(scanner.Text())
-		}
-	}()
+	logrusWriter := newLogrusWriter(logger)
+	defer logrusWriter.Close()
+	go io.Copy(io.MultiWriter(outfile, logrusWriter), sessionPipeReader)
 
 	logger.Info("Running loadtest")
 	if err := session.Run("cd mattermost-load-test && ./bin/loadtest all"); err != nil {
