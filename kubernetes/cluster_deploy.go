cb88b2e4080e6bbbf26ad505d142bff3b991f81e
Carlos Panato <ctadeu@gmail.com>
2019-05-20 11:10:02 +0200

    update helm repo and chart name

--- kubernetes/cluster_deploy.go
+++ kubernetes/cluster_deploy.go
@@ -45,26 +45,26 @@ func (c *Cluster) Deploy(options *ltops.DeployOptions) error {
 	}
 
 	if previouslyDeployed {
-		cmd := exec.Command("helm", "upgrade", "-f", configFileLocation, c.Release(), "mattermost/mattermost-helm")
+		cmd := exec.Command("helm", "upgrade", "-f", configFileLocation, c.Release(), "mattermost/mattermost-enterprise-edition")
 		if out, err := cmd.CombinedOutput(); err != nil {
 			return errors.Wrap(err, "unable to install mattermost chart, error from helm: "+string(out))
 		}
 
 		// Delete the app pods to make sure they reset caches and restart to pick up config changes
-		cmd = exec.Command("kubectl", "delete", "po", "-l", fmt.Sprintf("release=%v,app=mattermost-helm", c.Release()))
+		cmd = exec.Command("kubectl", "delete", "po", "-l", fmt.Sprintf("release=%v,app=mattermost-enterprise-edition", c.Release()))
 		if out, err := cmd.CombinedOutput(); err != nil {
 			return errors.Wrap(err, "unable to restart app server pods, error from kubectl: "+string(out))
 		}
 
 		// Delete the loadtest pods to make sure they pick up config changes
-		cmd = exec.Command("kubectl", "delete", "po", "-l", fmt.Sprintf("release=%v,app=mattermost-helm-loadtest", c.Release()))
+		cmd = exec.Command("kubectl", "delete", "po", "-l", fmt.Sprintf("release=%v,appmattermost-enterprise-edition-loadtest", c.Release()))
 		if out, err := cmd.CombinedOutput(); err != nil {
 			return errors.Wrap(err, "unable to restart loadtest pods, error from kubectl: "+string(out))
 		}
 
 		log.Info("upgraded release '" + c.ReleaseName + "'")
 	} else {
-		cmd := exec.Command("helm", "install", "-n", c.Config.Name, "-f", configFileLocation, "mattermost/mattermost-helm")
+		cmd := exec.Command("helm", "install", "-n", c.Config.Name, "-f", configFileLocation, "mattermost/mattermost-enterprise-edition")
 		out, err := cmd.CombinedOutput()
 		if err != nil {
 			return errors.Wrap(err, "unable to install mattermost chart, error from helm: "+string(out))
